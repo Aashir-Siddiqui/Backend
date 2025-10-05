f24c72d38853585b631c02da6d8536f928a811fa
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-10-05 21:58:16 +0500

    Added delete add to favourite routes

--- airbnb/routes/storeRouter.js
+++ airbnb/routes/storeRouter.js
@@ -8,5 +8,8 @@ storeRouter.get("/", homesController.getIndex);
 storeRouter.get("/homes", homesController.getHomes);
 storeRouter.get("/bookings", homesController.getBookings);
 storeRouter.get("/favourites", homesController.getFavourites);
+storeRouter.post("/favourites", homesController.addPostFavourites)
+storeRouter.post("/favourites/delete/:homeId", homesController.postRemoveFromFavourites)
+storeRouter.get("/homes/:homeId", homesController.getHomeDetails);
 
 module.exports = storeRouter;
