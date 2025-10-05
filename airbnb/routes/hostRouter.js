093e05eef52f68f883cb7c1195192c016f360fc8
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-10-05 21:57:56 +0500

    Added delete add to favourite routes

--- airbnb/routes/hostRouter.js
+++ airbnb/routes/hostRouter.js
@@ -7,5 +7,8 @@ const hostController  = require("../controllers/hostController");
 hostRouter.get("/add-home", hostController.getAddHome);
 hostRouter.get("/host-home-list", hostController.getHostHome);
 hostRouter.post("/add-home", hostController.postAddHome);
+hostRouter.get("/edit-home/:homeId", hostController.getEditHome);
+hostRouter.post("/edit-home", hostController.postEditHome);
+hostRouter.post("/delete-home/:homeId", hostController.postDeleteHome);
 
 module.exports = hostRouter;
