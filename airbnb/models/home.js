f43b5ec189d089a9a40389826ae2962d56a0e7c2
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-10-05 21:57:26 +0500

    Added delete add to favourite functionality

--- airbnb/models/home.js
+++ airbnb/models/home.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const rootDir = require("../utils/pathUtil");
 
+const homeDataPath = path.join(rootDir, "data", "homes.json");
+
 module.exports = class Home {
   constructor(houseName, address, price, rating, photoUrl) {
     this.houseName = houseName;
@@ -13,8 +15,14 @@ module.exports = class Home {
 
   save() {
     Home.fetchAll((registerHomes) => {
-      registerHomes.push(this);
-      const homeDataPath = path.join(rootDir, "data", "homes.json");
+      if (this.id) {
+        registerHomes = registerHomes.map((home) =>
+          home.id === this.id ? this : home
+        );
+      } else {
+        this.id = Math.random().toString();
+        registerHomes.push(this);
+      }
       fs.writeFile(homeDataPath, JSON.stringify(registerHomes), (err) => {
         console.log("File written concluded", err);
       });
@@ -22,9 +30,22 @@ module.exports = class Home {
   }
 
   static fetchAll(callback) {
-    const homeDataPath = path.join(rootDir, "data", "homes.json");
     fs.readFile(homeDataPath, (err, data) => {
       callback(!err ? JSON.parse(data) : []);
+    });
+  }
+
+  static findById(homeId, callback) {
+    this.fetchAll((homes) => {
+      const homeFound = homes.find((home) => home.id === homeId);
+      callback(homeFound);
+    });
+  }
+
+  static deleteById(homeId, callback) {
+    this.fetchAll((homes) => {
+      homes = homes.filter((home) => home.id !== homeId);
+      fs.writeFile(homeDataPath, JSON.stringify(homes), callback);
     });
   }
 };
