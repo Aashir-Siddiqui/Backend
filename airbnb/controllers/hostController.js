2a1f6dc472f0306f543c948887bdf23e102a965b
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-10-05 21:56:32 +0500

    Added delete add to favourite functionality

--- airbnb/controllers/hostController.js
+++ airbnb/controllers/hostController.js
@@ -1,7 +1,31 @@
 const Home = require("../models/home");
 
 exports.getAddHome = (req, res, next) => {
-  res.render("host/add-home", { pageTitle: "Add Home", currentPage: "addHome" });
+  res.render("host/edit-home", {
+    pageTitle: "Add Home",
+    currentPage: "addHome",
+    editing: false,
+  });
+};
+
+exports.getEditHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  const editMode = req.query.editing === "true";
+  console.log(homeId, editMode);
+
+  Home.findById(homeId, (home) => {
+    if (!home) {
+      console.log("Home not found for editing");
+      return res.redirect("/host/host-home-list");
+    }
+    console.log("Editing Home:", home);
+    res.render("host/edit-home", {
+      home: home,
+      pageTitle: "Edit Home",
+      currentPage: "addHome",
+      editing: editMode,
+    });
+  });
 };
 
 exports.postAddHome = (req, res, next) => {
@@ -9,7 +33,16 @@ exports.postAddHome = (req, res, next) => {
   const { houseName, address, price, rating, photoUrl } = req.body;
   const home = new Home(houseName, address, price, rating, photoUrl);
   home.save();
-  res.render("host/home-added", { pageTitle: "Home Added", currentPage: "homeAdded" });
+  res.redirect("/host/host-home-list");
+};
+
+exports.postEditHome = (req, res, next) => {
+  console.log(req.body);
+  const { id, houseName, address, price, rating, photoUrl } = req.body;
+  const home = new Home(houseName, address, price, rating, photoUrl);
+  home.id = id;
+  home.save();
+  res.redirect("/host/host-home-list");
 };
 
 exports.getHostHome = (req, res, next) => {
@@ -20,4 +53,15 @@ exports.getHostHome = (req, res, next) => {
       currentPage: "host-home",
     })
   );
+};
+
+exports.postDeleteHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  console.log("Deleting Home ID:", homeId);
+  Home.deleteById(homeId, (error) => {
+    if (error) {
+      console.log("Error deleting home:", error);
+    }
+    res.redirect("/host/host-home-list");
+  });
 };
